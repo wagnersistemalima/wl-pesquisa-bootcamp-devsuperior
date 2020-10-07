040a1584e8b7712560ca1b4d8664d5d21973304f
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-10-06 21:22:05 -0300

    Project created

---
+++ backend/src/main/java/com/sistemalima/wlpesquisa/WlpesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.sistemalima.wlpesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlpesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlpesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sistemalima/wlpesquisa/WlpesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemalima.wlpesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlpesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
