254beea3c163aa1fc76f30ea5a2d6fb278b2e319
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-10-07 23:04:51 -0300

    Find Client by id, exception handling

--- backend/src/main/java/com/sistemalima/wlpesquisa/services/ClientService.java
+++ backend/src/main/java/com/sistemalima/wlpesquisa/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.sistemalima.wlpesquisa.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.sistemalima.wlpesquisa.dto.ClientDTO;
 import com.sistemalima.wlpesquisa.entities.Client;
 import com.sistemalima.wlpesquisa.repositories.ClientRepository;
+import com.sistemalima.wlpesquisa.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -22,6 +24,14 @@ public class ClientService {
 		List<Client> list = repository.findAll();
 		
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)                 // metodo para buscar clientes por id
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
+		
 	}
 
 }
