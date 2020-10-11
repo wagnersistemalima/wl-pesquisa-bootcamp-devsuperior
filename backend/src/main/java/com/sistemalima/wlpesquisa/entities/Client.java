1507a87dc7a7060632de2761778df86fc4f2f478
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-10-10 21:27:35 -0300

    Annotation Instant birthDate

--- backend/src/main/java/com/sistemalima/wlpesquisa/entities/Client.java
+++ backend/src/main/java/com/sistemalima/wlpesquisa/entities/Client.java
@@ -3,6 +3,7 @@ package com.sistemalima.wlpesquisa.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,7 +22,7 @@ public class Client implements Serializable{
 	private String cpf;
 	private Double income;
 	
-	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	
 	private Integer children;
